03b1eae5e47312e799b4da6b1af7757d16bcabce
Daniel Amesberger <daniel.amesberger@amescon.com>
2023-05-09 20:45:46 +0200

    GraphQL Schema new getValue and getValueProperties

--- src/connectors/server/schema.ts
+++ src/connectors/server/schema.ts
@@ -237,11 +237,7 @@ export class PilotNodeGraphQLSchema {
             resolve: async (source: any, args: any) => {
                 if (args.name && args.name in this.valueService.valuelookup) {
                     if (args.subValue in this.valueService.valuelookup[args.name].values) {
-                        if (args.value) {
-                            return this.valueService.valuelookup[args.name].values[args.subValue].getValue();
-                        } else {
-                            throw new Error('value not found')
-                        }
+                        return this.valueService.valuelookup[args.name].values[args.subValue].getValue();
                     } else {
                         throw new Error('subValue not found');
                     }
@@ -260,11 +256,7 @@ export class PilotNodeGraphQLSchema {
             resolve: async (source: any, args: any) => {
                 if (args.name && args.name in this.valueService.valuelookup) {
                     if (args.subValue in this.valueService.valuelookup[args.name].values) {
-                        if (args.value) {
-                            return this.valueService.valuelookup[args.name].values[args.subValue].properties;
-                        } else {
-                            throw new Error('value not found')
-                        }
+                        return this.valueService.valuelookup[args.name].values[args.subValue].properties;
                     } else {
                         throw new Error('subValue not found');
                     }
